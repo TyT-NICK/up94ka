2b2122749b67de88fb896d0a2102f7165d9ebada
TyT-NICK <artem99k14@gmail.com>
2021-05-24 18:13:14 +0800

    server initial setup

--- up94ka-server/index.js
+++ up94ka-server/index.js
@@ -1,1 +1,11 @@
-console.log(process.env.Test);
+const app = require('express')();
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 8080;
+
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`server on http://127.0.0.1:${PORT}`);
+});
