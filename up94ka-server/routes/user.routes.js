f9dfacb64a8d8e9e534ffd893bea43acb1fae8a3
TyT-NICK <artem99k14@gmail.com>
2021-05-30 22:41:36 +0800

    Added getting notifications

--- up94ka-server/routes/user.routes.js
+++ up94ka-server/routes/user.routes.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 const User = require('../models/user');
 const messages = require('../messages');
 const utils = require('../utils/utils');
+const Notifications = require('../models/notifications');
+
+router.get('/get-notifications', async (req, res) => {
+  const { user } = req.session;
+  if (!user) return res.status(403).send(messages.loginNeeded);
+
+  const { role } = user;
+
+  const notifications = await Notifications.find({ to: 0 });
+  if (role >= 1) notifications.push(...await Notifications.find({ to: 1 }));
+  if (role === 2) notifications.push(...await Notifications.find({ to: 2 }));
+
+  return res.json(notifications);
+});
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
@@ -10,7 +24,7 @@ router.get('/:id', async (req, res) => {
 
   if (!user) return res.status(404).send(messages.userNotFound);
 
-  return res.json(utils.removeFieldsFromObject(user._doc, 'password', 'role'));
+  return res.send(utils.removeFieldsFromObject(user._doc, 'password', 'role'));
 });
 
 module.exports = router;
